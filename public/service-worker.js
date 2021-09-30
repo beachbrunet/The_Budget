5c94526124ab51119631aff67ed7ad70258c8d0c
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-30 13:55:11 -0600

    added psuedo code

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+console.log("Hello from service worker!");
+
+// Static cache
+
+// data caches
+
+// files to caches
+
+// install
+
+// tell browser to activate to this js file (once its done installing)
+
+//remove old cache
+
+//
+
+//
+// if request fails, serve static files from the cache -(allows page to be accessible offline)
