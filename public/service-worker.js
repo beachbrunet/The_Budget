bfa91aedf0e368e03c2cda255e8ed9aff6ce665a
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-30 14:02:23 -0600

    editing manifest.webmani

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 console.log("Hello from service worker!");
 
 // Static cache
-
+const CACHE_NAME = "static-cache-v2";
 // data caches
-
+const DATA_CACHE_NAME = "data-cache-v1";
 // files to caches
-
+const FILES_TO_CACHE = [];
 // install
 
 // tell browser to activate to this js file (once its done installing)
