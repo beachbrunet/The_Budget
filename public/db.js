bbda45d5e148e5ce95c9333143d576168893ef98
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 11:07:16 -0600

    added db.js and added psuedo code

---
+++ public/db.js
@@ -0,0 +1,8 @@
+let db;
+// create a request for the budget db --connect
+
+// create schema and store data
+
+// check if app is online before it reads the db
+
+// look to see if the db is back online
