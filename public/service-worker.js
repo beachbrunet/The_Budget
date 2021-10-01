be5753e67234d53884eee11b41884bdd1186fa6a
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 10:12:59 -0600

    working on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,8 +15,34 @@ const FILES_TO_CACHE = [
 ];
 // install
 self.addEventListener("install", function (evt) {
-  //
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  // Service worker will skip waiting phase and be immediately activated
+  self.skipWaiting();
 });
+
+// activate service worker --added code from exmaple
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
 // tell browser to activate to this js file (once its done installing)
 
 //remove old cache
