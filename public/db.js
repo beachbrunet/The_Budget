fc918a0ef5436712fb0b96fbbf796e11ef28a081
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 11:17:44 -0600

    saving function

--- public/db.js
+++ public/db.js
@@ -1,7 +1,23 @@
+// create variable to chold connection
 let db;
-// create a request for the budget db --connect
+// create a request for the budget db, set to version 1 --connect
+const request = indexedDB.open("budget", 1);
 
 // create schema and store data
+request.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  //   create an object store table and set it to an autoincrement
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+// if successful -- establish connection and save to the db variable.
+request.onsuccess = function (e) {
+  db = e.target.result;
+  //   Check to see if its online, if yes then send data to the api.
+  if (navigator.online) {
+    uploadTransation();
+  }
+};
 
 // check if app is online before it reads the db
 
